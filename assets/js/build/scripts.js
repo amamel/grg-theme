b2c12c026dbbccf6b710d5605f773692aabf99a8
mamela <alex@alexmamel.com>
2016-08-30 22:43:32 -0400

    content chunks > blocks

--- assets/js/build/scripts.js
+++ assets/js/build/scripts.js
@@ -874,3 +874,30 @@
 	}
 })();
 
+// Attach Fastclick JS to document body
+$(function() {
+    FastClick.attach(document.body);
+});
+
+$(document).ready( function() {
+
+	// Responsive Navigation JS
+	var $nav_main = $('.site-nav'),
+		  $nav_trigger = $('.nav-trigger'),
+      $subnav_trigger = $('.menu-item-has-children > a');
+
+	$nav_trigger.on('click',function() {
+		$nav_main.toggleClass('open');
+		$nav_trigger.toggleClass('open');
+		return false;
+	});
+
+	$subnav_trigger.on('click',function(e) {
+		e.preventDefault();
+    console.log(e);
+		var $this = $(this);
+		$this.toggleClass('open').next('ul').toggleClass('open');
+	});
+
+});
+
